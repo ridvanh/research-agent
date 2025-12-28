d79bf41633778806906cb2c5497d6d4b097529d3
ridvanh <ridvanhocaoglu1@gmail.com>
2025-12-28 19:16:58 +0300

    Initial commit

--- test_summary.py
+++
@@ -1,9 +0,0 @@
-from research_agent import search_arxiv_papers, summarize_paper
-
-papers = search_arxiv_papers("transformer architecture")
-
-for paper in papers:
-    print(f"\nTitle: {paper['title']}")
-    print("Summary:\n")
-    print(summarize_paper(paper))
-    print("\n" + "-"*80 + "\n")
